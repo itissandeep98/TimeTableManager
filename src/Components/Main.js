8e36b7221353a460ee181818c75eefee8eb919e3
Sandeep <itissandeep98@gmail.com>
2020-06-21 12:21:45 +0530

    intial configurations

---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Time Table Manager</h1>
+			</div>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>
