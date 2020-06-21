3bb012e67aa0b50ffbf61acd1d89d5ef350ec9fd
Sandeep <itissandeep98@gmail.com>
2020-06-21 12:26:32 +0530

    Updates

---
+++ precache-manifest.6df5e2e7b92e890c8e3fd42a8ff8e54a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d36e68c423e02ddf27e7518de84440",
+    "url": "/TimeTableManager/index.html"
+  },
+  {
+    "revision": "1137f344bb4d851b5d96",
+    "url": "/TimeTableManager/static/css/2.d60cf902.chunk.css"
+  },
+  {
+    "revision": "deebf15e88db8ffd5669",
+    "url": "/TimeTableManager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1137f344bb4d851b5d96",
+    "url": "/TimeTableManager/static/js/2.053da5ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TimeTableManager/static/js/2.053da5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deebf15e88db8ffd5669",
+    "url": "/TimeTableManager/static/js/main.37b01117.chunk.js"
+  },
+  {
+    "revision": "8976cf7b17c0a9b3ea6a",
+    "url": "/TimeTableManager/static/js/runtime-main.e8c87b5c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/TimeTableManager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/TimeTableManager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/TimeTableManager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/TimeTableManager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/TimeTableManager/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/TimeTableManager/precache-manifest.6df5e2e7b92e890c8e3fd42a8ff8e54a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TimeTableManager/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
