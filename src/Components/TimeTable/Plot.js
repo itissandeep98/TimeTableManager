69b453f1dd0b3cbd69af1858f5e4ea706a349feb
itissandeep98 <itissandeep98@gmail.com>
2020-07-07 02:04:02 +0530

    table beautified

--- src/Components/TimeTable/Plot.js
+++ src/Components/TimeTable/Plot.js
@@ -8,11 +8,11 @@ export default class Plot extends Component {
 		super(props);
 		var schedule = [
 			["", "9:00-10:30", "10:30-12", "12:00-1:30", "1:30-2:30", "2:30-4:00", "4:00-5:30"],
-			["Mon", "", "", "", "", "", "",],
-			["Tue", "", "", "", "", "", "",],
-			["Wed", "", "", "", "", "", "",],
-			["Thu", "", "", "", "", "", "",],
-			["Fri", "", "", "", "", "", "",]
+			["Mon", "", "", "", "L", "", "",],
+			["Tue", "", "", "", "U", "", "",],
+			["Wed", "", "", "", "N", "", "",],
+			["Thu", "", "", "", "C", "", "",],
+			["Fri", "", "", "", "H", "", "",]
 		]	
 		this.state={
 			courses:[],
@@ -28,7 +28,7 @@ export default class Plot extends Component {
 		var schedule=this.props.schedule;
 		var pos=[]
 		for (let i = 1; i <= 5; i++) {
-			for (let j = 1; j <= 5; j++) {
+			for (let j = 1; j <= 6; j++) {
 				if(schedule[i.toString()][j.toString()].includes(course)){
 					pos.push([i,j])
 				}
@@ -40,17 +40,18 @@ export default class Plot extends Component {
 	updateSchedule(){
 		var temp = [
 			["", "9:00-10:30", "10:30-12", "12:00-1:30", "1:30-2:30", "2:30-4:00", "4:00-5:30"],
-			["Mon", "", "", "", "", "", "",],
-			["Tue", "", "", "", "", "", "",],
-			["Wed", "", "", "", "", "", "",],
-			["Thu", "", "", "", "", "", "",],
-			["Fri", "", "", "", "", "", "",]
+			["Mon", "", "", "", "L", "", "",],
+			["Tue", "", "", "", "U", "", "",],
+			["Wed", "", "", "", "N", "", "",],
+			["Thu", "", "", "", "C", "", "",],
+			["Fri", "", "", "", "H", "", "",]
 		]
 		
 			for (let i = 0; i < this.state.courses.length; i++) {
 				var allpos=this.findpos(this.state.courses[i])
 				for (let pos = 0; pos < allpos.length; pos++) {
 					temp[allpos[pos][0]][allpos[pos][1]] = temp[allpos[pos][0]][allpos[pos][1]].concat(this.state.courses[i])
+					temp[allpos[pos][0]][allpos[pos][1]] = temp[allpos[pos][0]][allpos[pos][1]].concat(", ")
 				}
 			}
 			
