579021368f46bed0372cfb9bc19bf8f1ef1e5328
itissandeep98 <itissandeep98@gmail.com>
2020-07-13 01:42:50 +0530

    toggle added

--- src/Components/TimeTable/Plot.js
+++ src/Components/TimeTable/Plot.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import TableData from './Table'
-import { Form, Dropdown } from 'semantic-ui-react'
+import { Form, Dropdown, Checkbox, Segment } from 'semantic-ui-react'
 import { Spinner } from 'reactstrap';
 
 export default class Plot extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		var schedule = [
 			["", "9:00-10:30", "10:30-12", "12:00-1:30", "1:30-2:30", "2:30-4:00", "4:00-5:30"],
@@ -13,30 +13,31 @@ export default class Plot extends Component {
 			["Wed", "", "", "", "N", "", "",],
 			["Thu", "", "", "", "C", "", "",],
 			["Fri", "", "", "", "H", "", "",]
-		]	
-		this.state={
-			courses:[],
+		]
+		this.state = {
+			courses: [],
 			schedule,
+			season:true
 		}
-		this.handleCourseChange=this.handleCourseChange.bind(this);
-		this.updateSchedule=this.updateSchedule.bind(this);
-		this.findpos=this.findpos.bind(this);
+		this.handleCourseChange = this.handleCourseChange.bind(this);
+		this.updateSchedule = this.updateSchedule.bind(this);
+		this.findpos = this.findpos.bind(this);
 	}
 
-	findpos(course){
-		var schedule=this.props.schedule;
-		var pos=[]
+	findpos(course) {
+		var schedule = this.props.schedule;
+		var pos = []
 		for (let i = 1; i <= 5; i++) {
 			for (let j = 1; j <= 6; j++) {
-				if(schedule[i.toString()][j.toString()].includes(course)){
-					pos.push([i,j])
+				if (schedule[i.toString()][j.toString()].includes(course)) {
+					pos.push([i, j])
 				}
 			}
 		}
 		return pos
 
 	}
-	updateSchedule(){
+	updateSchedule() {
 		var temp = [
 			["", "9:00-10:30", "10:30-12", "12:00-1:30", "1:30-2:30", "2:30-4:00", "4:00-5:30"],
 			["Mon", "", "", "", "L", "", "",],
@@ -45,18 +46,18 @@ export default class Plot extends Component {
 			["Thu", "", "", "", "C", "", "",],
 			["Fri", "", "", "", "H", "", "",]
 		]
-		
+
 		for (let i = 0; i < this.state.courses.length; i++) {
-			var allpos=this.findpos(this.state.courses[i]) //get all coordinates for a particular course
+			var allpos = this.findpos(this.state.courses[i]) //get all coordinates for a particular course
 			for (let pos = 0; pos < allpos.length; pos++) { // update at all the coordinates
-				temp[allpos[pos][0]][allpos[pos][1]] = temp[allpos[pos][0]][allpos[pos][1]].concat(this.state.courses[i]) 
+				temp[allpos[pos][0]][allpos[pos][1]] = temp[allpos[pos][0]][allpos[pos][1]].concat(this.state.courses[i])
 				temp[allpos[pos][0]][allpos[pos][1]] = temp[allpos[pos][0]][allpos[pos][1]].concat(", ")
 			}
 		}
-			
+
 		this.setState({
-			schedule:temp
-		})		
+			schedule: temp
+		})
 	}
 
 	handleCourseChange(event, result) {
@@ -64,32 +65,56 @@ export default class Plot extends Component {
 		this.setState({
 			...this.state.courses,
 			courses: value
-		},()=>this.updateSchedule());
+		}, () => this.updateSchedule());
 	}
 
-	
+	onChange(){
+		this.setState({
+			season:!this.state.season,
+			courses:[]
+		})
+	}
+
+
 	render() {
-		var courselist=[
-			{key:"loading", value:<Spinner/>, text:<Spinner/>, disabled:true}
+		var courselist = [
+			{ key: "loading", value: <Spinner />, text: <Spinner />, disabled: true }
 		]
-		if(this.props.courselist)
-			courselist=this.props.courselist["monsoon"]
-		
+		if (this.props.courselist){
+			courselist = this.props.courselist["monsoon"]
+			if(this.state.season){
+				courselist = this.props.courselist["winter"]
+			}
+		}
+		console.log(courselist.length)
+
 		return (
 			<div className="container">
-				<Form>
-					<Form.Field>
-					<Dropdown 
-						placeholder="Select Courses" 
-						fluid multiple search openOnFocus 
-						clearable selection options={courselist} 
-						value={this.state.courses} 
-						onChange={this.handleCourseChange}
-					/>
-					</Form.Field>
-				</Form>
-				<hr/>
+
 				<TableData data={this.state.schedule} />
+				<hr />
+				<Segment>
+				<div className="text-center">
+					
+					<Form>
+						<Form.Field inline>
+							<label>Monsoon</label>
+							<Checkbox slider checked={this.state.season} onChange={this.onChange.bind(this)}/>
+							<label>Winter</label>
+						</Form.Field>
+					</Form>
+					
+				</div>
+
+				<Dropdown
+					placeholder="Select Courses"
+					fluid multiple search openOnFocus
+					clearable selection options={courselist}
+					value={this.state.courses}
+					onChange={this.handleCourseChange}
+				/>
+				</Segment>
+
 			</div>
 		)
 	}
