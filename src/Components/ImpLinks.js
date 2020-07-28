48ea99f3a2489b02b31de4394124328454cc5b0f
sandeep <itissandeep98@gmail.com>
2020-07-28 18:02:33 +0530

    links fixed

--- src/Components/ImpLinks.js
+++ src/Components/ImpLinks.js
@@ -7,13 +7,13 @@ export const ImpLink = () => {
 			<h2>Important Links</h2>
 			<ul>
 				<li>
-					<a href="https://drive.google.com/drive/folders/1YqptkWKt6iKvoImkJYRlB_2yRH69K_D2" target="_blank">Course Descriptions</a>
+					<a href="https://drive.google.com/drive/folders/1YqptkWKt6iKvoImkJYRlB_2yRH69K_D2" target="_blank" rel="noopener noreferrer">Course Descriptions</a>
 				</li>
 				<li>
-					<a href="https://docs.google.com/document/d/1v8EJcxPvXgtNYNFtnUjrvdiP3F5UZMM3vqw7HcDsN7A" target="_blank">Review of electives</a>
+					<a href="https://docs.google.com/document/d/1v8EJcxPvXgtNYNFtnUjrvdiP3F5UZMM3vqw7HcDsN7A" target="_blank" rel="noopener noreferrer">Review of electives</a>
 				</li>
 				<li>
-					Current Time Table <a href="https://iiitd.ac.in/academics/m2020" target="_blank">(Monsoon 2020)</a>
+					Current Time Table <a href="https://iiitd.ac.in/academics/m2020" target="_blank" rel="noopener noreferrer">(Monsoon 2020)</a>
 				</li>
 			</ul>
 
