285eddfa0196427e7b35f431d64242c785ede353
itissandeep98 <itissandeep98@gmail.com>
2020-08-05 14:19:29 +0530

    download and copy button

--- src/Components/Admin/Dataview.js
+++ src/Components/Admin/Dataview.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { FormGroup, Form } from 'reactstrap'
+import { Button } from 'semantic-ui-react';
+
 
 export default class Dataview extends Component {
 	constructor(props) {
@@ -22,11 +24,35 @@ export default class Dataview extends Component {
 		this.textInput.style.width = `${this.textInput.parentElement.clientWidth-20}px`
 	}
 
+	downloadFile(e,doc){
+		e.preventDefault()
+
+		const element = document.createElement("a");
+		const file = new Blob([doc], { type: 'text/plain' });
+		element.href = URL.createObjectURL(file);
+		element.download = "db.json";
+		document.body.appendChild(element); // Required for this to work in FireFox
+		console.log(element);
+		element.click();
+		
+	}
+
+	copyText(e){
+		e.preventDefault()
+		let text=this.textInput
+		text.select()
+		text.setSelectionRange(0, 99999); /*For mobile devices*/
+		document.execCommand("copy");		
+	}
+
 	render() {
 		let docs=JSON.stringify(this.props.doc,null,4)
 		
 		return (
 			<Form>
+				<Button onClick={(e)=>this.downloadFile(e,docs)}>Download</Button>
+				<Button onClick={(e)=>this.copyText(e)}>Copy</Button>
+				<br /><br />
 				<FormGroup>
 					<textarea  ref={this.setTextInputRef} value={docs} />
 				</FormGroup>
